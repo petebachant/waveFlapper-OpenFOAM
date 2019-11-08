9e6b596aa856e1d50a9c2c2882289cf1a5d8c0c0
Pete Bachant <pete@windesco.com>
2019-11-08 09:02:51 -0500

    Fix up plotting script

--- plot.py
+++ plot.py
@@ -7,33 +7,36 @@ import matplotlib.pyplot as plt
 
 width_2d = 0.1
 width_3d = 3.66
-m_paddle = 1270.0 # Paddle mass in kg, from OMB manual
-h_piston = 3.3147  
+m_paddle = 1270.0  # Paddle mass in kg, from OMB manual
+h_piston = 3.3147
 I_paddle = 1/3*m_paddle*h_piston**2
 
 
 def plot_force():
     """Plots the streamwise force on the paddle over time."""
+    pass
 
 
 def plot_moment():
-    data = foampy.load_forces_moments()
+    data = foampy.load_forces()
     i = 10
     t = data["time"][i:]
-    m = data["moment"]["pressure"]["z"] + data["moment"]["viscous"]["z"]
-    m = m[i:]*width_3d/width_2d
+    m = data.mz
+    m = m[i:] * width_3d / width_2d
     period = 2.2
-    omega = 2*np.pi/period
-    theta = 0.048*np.sin(omega*t)
-    theta_doubledot = -0.048*omega**2*np.sin(omega*t)
-    m_inertial = I_paddle*theta_doubledot
+    omega = 2 * np.pi / period
+    theta = 0.048 * np.sin(omega * t)
+    theta_doubledot = -0.048 * omega**2 * np.sin(omega * t)
+    m_inertial = I_paddle * theta_doubledot
     m = m + m_inertial
     plt.figure()
     plt.plot(t, m)
-    plt.xlabel("t (s)")
+    plt.xlabel("Time (s)")
     plt.ylabel("Flapper moment (Nm)")
-    print("Max moment from CFD (including inertia) = {:0.1f}".format(m.max()), "Nm")
-    print("Theoretical max moment (including inertia) =", 5500*3.3, "Nm") 
+    print(
+        "Max moment from CFD (including inertia) = {:0.1f} Nm".format(m.max())
+    )
+    print("Theoretical max moment (including inertia) =", 5500*3.3, "Nm")
     plt.show()
 
 
